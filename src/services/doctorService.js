cf1c9881ac2a059756659a7c8def992234cde902
hothamphong <hothamphong21031999@gmail.com>
2022-09-21 12:47:30 +0700

    #76

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -153,16 +153,9 @@ let bulkCreateSchedule = (data) => {
                     attributes: ['timeType', 'date', 'doctorId', 'maxNumber'],
                     raw: true
                 });
-                //convert date
-                if (existing && existing.length > 0) {
-                    existing = existing.map(item => {
-                        item.date = new Date(item.date).getTime();
-                        return item;
-                    })
-                }
                 //compare different
                 let toCreate = _.differenceWith(schedule, existing, (a, b) => {
-                    return a.timeType === b.timeType && a.date == b.date;
+                    return a.timeType === b.timeType && +a.date === +b.date;
                 });
                 //create data
                 if (toCreate && toCreate.length > 0) {
@@ -193,6 +186,11 @@ let getScheduleByDate = (doctorId, date) => {
                         doctorId: doctorId,
                         date: date
                     },
+                    include: [
+                        { model: db.Allcode, as: 'timeTypeData', attributes: ['valueEn', 'valueVi'] },
+                    ],
+                    raw: false,
+                    nest: true
                 })
                 if (!dataSchedule) dataSchedule = [];
                 resolve({
